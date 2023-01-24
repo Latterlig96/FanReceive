7d234e0b7546f924318b0de7aa2aabee13860072
Latterlig96 <47533222+Latterlig96@users.noreply.github.com>
2023-01-24 19:43:30 +0100

    feat: Preparing build scripts for aws beanstalk (#2)
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * feat: Preparing beanstalk deployment
    
    * updates
    
    * eb updates
    
    * eb updates after mistake
    
    * eb updates
    
    * eb updates
    
    * eb updates
    
    * eb updates
    
    * eb updates
    
    * eb updates
    
    * eb updates
    
    * eb updates
    
    * eb updates

---
+++ fanreceive/users/management/commands/createsu.py
@@ -0,0 +1,19 @@
+from django.core.management.base import BaseCommand
+from django.contrib.auth import get_user_model
+
+class Command(BaseCommand):
+    help = 'Creates a superuser.'
+
+    def handle(self, *args, **options):
+        User = get_user_model()
+        if not User.objects.filter(username='testUser').exists():
+            User.objects.create_superuser(
+                username='testUser',
+                first_name="testname",
+                last_name="testsurname",
+                email="testUser@gmail.com",
+                age=20,
+                city="Cracow",
+                password='testpassword'
+            )
+        print('Superuser has been created.')
