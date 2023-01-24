95540dbe85c15626aae30e37d0bac65e0ed7d869
Patryk <patryk.zajac05@gmail.com>
2023-01-24 19:31:20 +0100

    eb updates

--- fanreceive/users/management/commands/createsu.py
+++ fanreceive/users/management/commands/createsu.py
@@ -1,11 +1,12 @@
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
-
+from django.contrib.auth import get_user_model
 
 class Command(BaseCommand):
     help = 'Creates a superuser.'
 
     def handle(self, *args, **options):
+        User = get_user_model()
         if not User.objects.filter(username='testUser').exists():
             User.objects.create_superuser(
                 username='testUser',
