9d9630b9a64d023f0c1ca54cc8ce2fb3a3a55ac9
Patryk <patryk.zajac05@gmail.com>
2023-01-24 14:45:54 +0100

    feat: Preparing beanstalk deployment

---
+++ fanreceive/users/management/commands/createsu.py
@@ -0,0 +1,19 @@
+from django.contrib.auth.models import User
+from django.core.management.base import BaseCommand
+
+
+class Command(BaseCommand):
+    help = 'Creates a superuser.'
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username='testUser').exists():
+            User.objects.create_superuser(
+                username='testUser',
+                first_name="testname",
+                last_name="testsurname",
+                email="testUser@gmail.com",
+                age=20,
+                city="Cracow",
+                password='testpassword'
+            )
+        print('Superuser has been created.')
