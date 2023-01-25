bcaf3949aadc2013ff8afe3eb80db7bbe30dcc88
Patryk <patryk.zajac05@gmail.com>
2023-01-25 19:56:43 +0100

    CI fix

--- fanreceive/fanreceive/settings.py
+++ fanreceive/fanreceive/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    'ebhealthcheck.apps.EBHealthCheckConfig',
     "rest_framework",
     "rest_framework_simplejwt",
     "corsheaders",
