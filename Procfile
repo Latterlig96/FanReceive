ac34aa6289966d00688cc358544df45a8fbece9e
Patryk <patryk.zajac05@gmail.com>
2023-01-25 18:54:48 +0100

    eb updates

--- Procfile
+++ Procfile
@@ -1,5 +1,3 @@
 web: gunicorn --bind 127.0.0.1:8000 --workers=1 --threads=15 fanreceive.wsgi:application
-
 celery_worker: celery worker -A fanreceive.settings.celery.app --loglevel=INFO
-
 celery_beat: celery beat -A fanreceive.settings.celery.app --loglevel=INFO
--- fanreceive/fanreceive/settings.py
+++ fanreceive/fanreceive/settings.py
@@ -145,6 +145,7 @@ if "ELASTICACHE_URL_BACKEND" in os.environ:
 else:
     CELERY_BROKER_URL = "redis://redis:6379"
     CELERY_RESULT_BACKEND = "redis://redis:6379/0"
+    
 CELERY_TIMEZONE = "Europe/Warsaw"
 CELERY_TASK_TRACK_STARTED = True
 CELERY_TASK_TIME_LIMIT = 30 * 60
